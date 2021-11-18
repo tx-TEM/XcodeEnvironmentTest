3e50123cbefb20610534bc5974572959411b6c78
tx-TEM <highhigheggman@gmail.com>
2021-11-18 19:17:44 +0900

    Initial Commit

---
+++ aozora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aozora
+//
+//  Created by yoshiki-t on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
