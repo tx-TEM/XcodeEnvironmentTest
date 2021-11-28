5247e0220375ffd5293a33cd13e03c7a274bcd1c
tx-TEM <highhigheggman@gmail.com>
2021-11-28 15:23:08 +0900

    RootVCにtableViewを配置した

--- aozora/Source/ViewController/RootViewController.swift
+++ aozora/Source/ViewController/RootViewController.swift
@@ -1,10 +1,24 @@
 import UIKit
 
 class RootViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     let viewModel = RootViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate =  self
+        tableView.dataSource = self
     }
 }
 
+extension RootViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+}
